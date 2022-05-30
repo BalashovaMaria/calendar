4e8ff6cf77a18cb1ddc65d7c4fc09e7c6acdd14a
dariatsobenko <99836236+dariatsobenko@users.noreply.github.com>
2022-05-30 14:16:08 +0300

    Update database.py

--- final/database.py
+++ final/database.py
@@ -4,6 +4,7 @@ import os
 # Класс для создания базы данных
 class BaseHandler:
     def __init__(self, path):
+        # Создает таблицу под названием 'records' для записи в нее данных, создаются столбцы текстового формата для распределения данных в них
         BASE_DIR = os.path.dirname(os.path.abspath(__file__))
         self._path = os.path.join(BASE_DIR, path)
         if not os.path.exists(self._path):
@@ -17,16 +18,19 @@ class BaseHandler:
             self._cur = self._conn.cursor()
 
     def add_record(self, name: str, dosage: str, times: list, date: str):
+        # Добавляет данные в таблицу 'records'
         for time in times:
             s = f"INSERT INTO RECORDS(name, dosage, time, date) VALUES('{name}','{dosage}','{time}','{date}')"
             self._cur.execute(s)
         self._conn.commit()
 
     def find(self, date):
+        # Находит соответствующую дату для запроса записей соответствующих данных
         self._cur.execute(f"SELECT * FROM RECORDS WHERE date = '{date}';")
         return self._cur.fetchall()
 
     def get_all(self):
+        # Передает сохраненные данные из базы данных в дочернее окно при последующих его вызовах
         self._cur.execute(f"SELECT * FROM RECORDS;")
         return self._cur.fetchall()
 
